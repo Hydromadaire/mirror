fb7c7d4d2ecaff59ddb7275a21e4eba59ff83d52
Tom Tzook <tomtzook@gmail.com>
2018-08-09 10:23:28 +0300

    added definisions to annotation

--- src/main/java/com/mirror/MirroredClass.java
+++ src/main/java/com/mirror/MirroredClass.java
@@ -1,5 +1,12 @@
 package com.mirror;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface MirroredClass {
     String value();
 }
