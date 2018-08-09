20eeae06c794ed73aa5b7b6f29facba7446f3b99
Tom Tzook <tomtzook@gmail.com>
2018-08-09 10:09:30 +0300

    added mirror

--- src/main/java/com/mirror/Mirror.java
+++ src/main/java/com/mirror/Mirror.java
@@ -1,4 +1,33 @@
 package com.mirror;
 
-public class Mirror {
+import com.mirror.helper.InvocationHelper;
+
+import java.lang.reflect.Proxy;
+
+public class Mirror<T> {
+
+    private Class<?> mTargetClass;
+    private Class<T> mMirrorClass;
+    private InvocationHelper mInvocationHelper;
+
+    public Mirror(Class<T> mirrorClass, Class<?> targetClass, InvocationHelper invocationHelper) {
+        mMirrorClass = mirrorClass;
+        mTargetClass = targetClass;
+        mInvocationHelper = invocationHelper;
+    }
+
+    public T create(Object instance) {
+        if (!mTargetClass.isInstance(instance)) {
+            throw new IllegalArgumentException("instance is not of targetClass type: " + mTargetClass.getName());
+        }
+
+        return createProxy(instance);
+    }
+
+    private T createProxy(Object instance) {
+        return (T) Proxy.newProxyInstance(
+                mMirrorClass.getClassLoader(),
+                new Class[] {mMirrorClass},
+                new MirrorInvocationHandler(mInvocationHelper, mTargetClass, instance));
+    }
 }
