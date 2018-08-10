1f3d0c1ad6685c6cf2d52eb3ea64e560e1dc4da6
Tom Tzook <tomtzook@gmail.com>
2018-08-10 12:19:51 +0300

    extracted validation to another class

--- src/main/java/com/mirror/MirrorCreator.java
+++ src/main/java/com/mirror/MirrorCreator.java
@@ -10,22 +10,25 @@ public class MirrorCreator {
     private final ClassLoader mClassLoader;
     private final MirrorHelper mMirrorHelper;
     private final InvocationHelper mInvocationHelper;
+    private final MirrorValidator mMirrorValidator;
 
-    public MirrorCreator(ClassLoader classLoader, MirrorHelper mirrorHelper, InvocationHelper invocationHelper) {
+    public MirrorCreator(ClassLoader classLoader, MirrorHelper mirrorHelper, InvocationHelper invocationHelper, MirrorValidator mirrorValidator) {
         mClassLoader = classLoader;
         mMirrorHelper = mirrorHelper;
         mInvocationHelper = invocationHelper;
+        mMirrorValidator = mirrorValidator;
     }
 
     private MirrorCreator(ClassLoader classLoader) {
         mClassLoader = classLoader;
         mMirrorHelper = new MirrorHelper();
         mInvocationHelper = new InvocationHelper(new Wrapper(mMirrorHelper, this), new Unwrapper(mMirrorHelper, mClassLoader));
+        mMirrorValidator = new MirrorValidator(mMirrorHelper);
     }
 
     public <T> Mirror<T> createMirror(Class<T> mirrorClass) throws MirrorCreationException {
         try {
-            validateMirrorClass(mirrorClass);
+            mMirrorValidator.validateMirrorClass(mirrorClass);
             Class<?> targetClass = getTargetType(mirrorClass);
             return new Mirror<T>(mirrorClass, targetClass, mInvocationHelper);
         } catch (ClassNotFoundException | ClassNotMirrorException | MirrorValidationException e) {
@@ -36,16 +39,6 @@ public class MirrorCreator {
     private Class<?> getTargetType(Class<?> mirrorClass) throws ClassNotFoundException {
         String targetTypeName = mMirrorHelper.getMirroredTypeName(mirrorClass);
         return Class.forName(targetTypeName, true, mClassLoader);
-    }
-
-    private void validateMirrorClass(Class<?> mirrorClass) {
-        if (!mMirrorHelper.isMirror(mirrorClass)) {
-            throw new ClassNotMirrorException(mirrorClass);
-        }
-
-        if (!mirrorClass.isInterface()) {
-            throw new MirrorValidationException("mirror should be an interface");
-        }
     }
 
     public static MirrorCreator createForClassLoader(ClassLoader classLoader) {
